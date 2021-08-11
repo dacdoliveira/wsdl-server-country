23f48aa666428c31d63b83e7f6ebb24a2a580db4
Diers <Diers@LAPTOP-VTCD2RNQ>
2021-08-11 10:43:50 -0300

    Sobe códigos gerados pelo plugin consumindo xsd

---
+++ src/main/java/com/dacdoliveira/wsdlservercountry/assets/Currency.java
@@ -0,0 +1,47 @@
+//
+// Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
+// Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
+// Gerado em: 2021.08.11 às 10:42:06 AM GFT 
+//
+
+
+package com.dacdoliveira.wsdlservercountry.assets;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java de currency.
+ * 
+ * <p>O seguinte fragmento do esquema especifica o conteúdo esperado contido dentro desta classe.
+ * <p>
+ * <pre>
+ * &lt;simpleType name="currency">
+ *   &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
+ *     &lt;enumeration value="GBP"/>
+ *     &lt;enumeration value="EUR"/>
+ *     &lt;enumeration value="BRL"/>
+ *   &lt;/restriction>
+ * &lt;/simpleType>
+ * </pre>
+ * 
+ */
+@XmlType(name = "currency")
+@XmlEnum
+public enum Currency {
+
+    GBP,
+    EUR,
+    BRL;
+
+    public String value() {
+        return name();
+    }
+
+    public static Currency fromValue(String v) {
+        return valueOf(v);
+    }
+
+}
