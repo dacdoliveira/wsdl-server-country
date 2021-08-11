4ca9836865ac9b68d210ea84df33c7d785ce55b7
Diers <Diers@LAPTOP-VTCD2RNQ>
2021-08-11 10:34:37 -0300

    Sobe versão inicial com configuração base do spring boot

---
+++ src/main/java/com/dacdoliveira/wsdlservercountry/WsdlServerCountryApplication.java
@@ -0,0 +1,13 @@
+package com.dacdoliveira.wsdlservercountry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsdlServerCountryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsdlServerCountryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dacdoliveira/wsdlservercountry/WsdlServerCountryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dacdoliveira.wsdlservercountry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsdlServerCountryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
